603f189bb2dc81da807c4459970982eaba7c1f95
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-04-24 17:38:12 +0200

    [FEATURE] Initial version of Bootstrap rendering

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,3 +4,4 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('microportals', 'Configuration/TypoScript/Textpic', 'Microportals - Textpic rendering');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('microportals', 'Configuration/TypoScript/Bootstrap', 'Microportals - Bootstrap rendering');
