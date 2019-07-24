0a4b6b093b7e28edb6ba7363b051d209b2655c15
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-07-24 11:52:44 +0200

    [TASK] Use seperate field for title override, cleanup

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,9 +1,9 @@
-
 #
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
-	tx_microportals_portalimage text,
-	tx_microportals_portalteaser text,
-	tx_microportals_portalicon varchar(255) DEFAULT '' NOT NULL
+	tx_microportals_portalicon VARCHAR(255) DEFAULT '' NOT NULL,
+	tx_microportals_portalimage TEXT,
+	tx_microportals_portalteaser TEXT,
+	tx_microportals_title_override VARCHAR(255) DEFAULT '' NOT NULL
 );
